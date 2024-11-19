f56c8ada7d7030429af195f01c5c48d156f13725
noaravski <noaravski@gmail.com>
2024-11-20 00:51:01 +0200

    add an api request - get all posts

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,3 +1,11 @@
 const Posts = require("../models/posts_model");
+const getAllPosts = async (req, res) => {
+  try {
+    const posts = await Posts.find();
+    res.send(posts);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+};
 
-module.exports = {};
+module.exports = { getAllPosts };
--- routes/posts_routes.js
+++ routes/posts_routes.js
@@ -1,5 +1,7 @@
-const express = require("express")
+const express = require("express");
 const router = express.Router();
-const postController = require("../controllers/posts_controller")
+const postController = require("../controllers/posts_controller");
+
+router.get("/", postController.getAllPosts);
 
 module.exports = router;
